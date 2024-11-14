c69df1084edcf07b1d107fa96980eaac9a08587c
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2024-11-14 11:44:37 +0900

    initial commit

---
+++ proc_box_pred_result.py
@@ -0,0 +1,76 @@
+
+
+#denoize したデータを使って、箱の情報を整理
+
+import os
+import pandas as pd
+import json
+
+
+#with open("denosed_pred_result_11-12_inspection_area.csv") as f:
+
+boxdf = pd.read_csv("denoised_pred_result_11-12_inspection_area.csv") 
+
+#　とりあえず、asayu の結果だけ使ってみる
+
+#json としては、以下の形式が望ましい？
+
+
+"""
+# box毎のフレーム情報
+boxes = [
+    [ // 各box 毎のin/out 情報 
+    {
+   start: frm,
+   end:   frm,
+   pallet_id: id,
+   pallet_type: type, // まあ知らんけど
+     },...
+  ]
+]
+"""
+
+
+print ("Max Box:",max(boxdf['box_no']))
+maxbox = max(boxdf['box_no'])
+
+boxdf.describe()
+
+boxdata = []
+for i in range(maxbox):
+    current_box_data = []
+    print ("Box:",i)
+    box = boxdf[boxdf['box_no']==i]
+    lastFrame = -10
+    pallet_id = 0
+    for idx, row in box.iterrows():
+        exist = row['pred_result']        
+        curframe = row['frame_id']
+        if exist==1 and lastFrame == -10: # 最初
+            start_frame = curframe
+            lastFrame = curframe
+        elif exist == 1 and lastFrame +1 == curframe: #前もある
+            lastFrame = curframe
+        elif exist == 1 : #飛んでいた場合
+            current_box_data.append(
+                {'start':start_frame,
+                 'end':lastFrame,
+                 'pallet_id': pallet_id,
+                 'pallet_type':row['original_pred_result']})
+            pallet_id += 1
+            start_frame = curframe
+            lastFrame = curframe
+    if lastFrame != -10:
+        current_box_data.append(
+            {'start':start_frame,
+             'end':lastFrame,
+             'pallet_id': pallet_id,
+             'pallet_type':row['original_pred_result']})
+        pallet_id += 1
+    print("Box ",i, len(current_box_data))
+    boxdata.append(current_box_data)
+
+        
+with open("box_inspection_area_11-12.json","w") as f:
+    json.dump(boxdata,f, indent=4)
+#
